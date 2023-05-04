0d8b4f6e00aada1a7ec5f5e8f6d6edc29738941b
Your Name <you@example.com>
2023-05-04 23:08:27 +0530

    fart

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String args[]){
-        System.out.println("Hello Aditya and Hello Ritesh");
+        System.out.println("Hello Aditya and Hello Ritesh betichod shurya suman");
     }
 }
         
