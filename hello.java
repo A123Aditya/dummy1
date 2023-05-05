7a98f549dfa3d4d1d6cdb9c2c50d30ac4906b1f5
K123Ritesh <you@example.com>
2023-05-05 22:13:59 +0530

    Chutiya

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class hello {
     public static void main(String args[]){
         System.out.println("Hello Aditya madharchod and Hello Ritesh");
         System.out.println("FGHGJUYFJ");
-        for(int i=1;i<10;i++){
+        for(int i=1;i<5;i++){
           System.out.println("Aditya MadharChod hai ");
           System.out.println("Aditya Dogla hai!!!");
-          System.out.println("Aditya Bhosriwala hai  !!!!");
+          System.out.println("Aditya Bhosriwala hai!!!!");
         }
     }
 }
